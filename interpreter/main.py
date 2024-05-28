b85bb354a8e4d84904b70cae3adce32973a679d6
ShauryaSen <shauryasen12@gmail.com>
2024-05-28 18:33:39 -0400

    fix relative pathing

--- interpreter/main.py
+++ interpreter/main.py
@@ -4,8 +4,8 @@ from interpreter import Interpreter
 
 
 if __name__ == "__main__":
-    program_path = "programs/HelloWorld"
+    program_path = "../programs/HelloWorld/New_folder"
     lexer = Lexer(program_path)
     tokens = lexer.tokenize()
-
-	print(tokens)
+        
+    print(tokens)
