c70c8d518aff8bd27c2143667e254ba90b55287d
Shaurya Sen <shauryasen12@gmail.com>
2024-05-28 14:30:00 -0400

    traverse folders

--- interpreter/lexer.py
+++ interpreter/lexer.py
@@ -6,3 +6,16 @@ class Lexer:
     Lexer walks through the folder structure and converts it into tokens 
     such as keywords, operators, and literals.
     """
+
+    def __init__(self, root_folder):
+        self.root_folder = root_folder
+        self.tokens = []
+
+    def traverse(self, folder):
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                # check for token
+                pass
+            for dir in dirs:
+                # check for token
+                pass
