4fbecb009d0254a68dd4f745dceffb0558b012fe
Shaurya Sen <shauryasen12@gmail.com>
2024-06-03 15:29:49 -0400

    remove interpreter and parser files

--- interpreter/interpreter.py
+++
@@ -1,5 +0,0 @@
-
-class Interpreter:
-    """
-    Traverses abstract syntax tree created by parser.py and executes the code.
-    """
--- interpreter/parser.py
+++
@@ -1,6 +0,0 @@
-
-class Parser:
-    """
-    Takes list of tokens created by lexer and creates a Abstract Syntax Tree so 
-    that it can be interpreted and executed.
-    """
