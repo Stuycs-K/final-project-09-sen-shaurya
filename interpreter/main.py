4f71ac46681df2cc10f5319e6795229f75cd27ab
Shaurya Sen <shauryasen12@gmail.com>
2024-06-03 15:30:03 -0400

    restructure main file

--- interpreter/main.py
+++ interpreter/main.py
@@ -1,11 +1,13 @@
 from lexer import Lexer
-from parser import Parser
-from interpreter import Interpreter
+from compiler import Compiler
 
 
 if __name__ == "__main__":
     program_path = "../programs/HelloWorld/"
+
     lexer = Lexer(program_path)
     tokens = lexer.lex()
-        
-    print(tokens)
+
+    Compiler = Compiler(tokens)
+    code = Compiler.compile()
+    
