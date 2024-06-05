209c5e3757867eef537b1002e027071f9d3d3edd
Shaurya Sen <shauryasen12@gmail.com>
2024-06-05 10:09:03 -0400

    Add input to compiler

--- interpreter/compiler.py
+++ interpreter/compiler.py
@@ -4,18 +4,32 @@ class Compiler:
     compiles tokens into code
     """
 
+    # COMPILED INPUT CODE #
+    # var0 = input()
+    # print(var0)
+
     def __init__(self, tokens):
         self.tokens = tokens
 
     def _compile_print(self, expression):
-        return f"print('{expression.value[1]}')\n"
+        if (expression.type == "LITERAL"):
+            if (expression.value[0] == "STRING"):
+                return f"print('{expression.value[1]}')\n"
+        elif (expression.type == "VARIABLE"):
+            return f"print({expression.value})\n"
+    
+    def _compile_input(self, expression):
+        return f"{expression} = input()\n"
 
     def compile(self):
         code = ""
         for token in self.tokens:
             command = token.type
             expression = token.value
+            if (command == "INPUT"):
+                code += self._compile_input(expression)
             if (command == "PRINT"):
                 code += self._compile_print(expression)
+            
         return code
 
